9a76b1c96f6763a9468406dd5862e11cc35ec358
Albert <assasinsalbert@gmail.com>
2024-01-15 18:40:42 +0500

    code completed

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,12 @@
 package com.epam.demo;
-
+import com.epam.utils.StringUtils;
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-       //magic happens here
+       for(String i : args)
+           if(!StringUtils.isPositiveNumber(i))
+               return false;
+       return true;
     }
 }
